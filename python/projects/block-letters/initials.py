4b8b94549fd893f1722308204ecd86cec87c7c58
dejisol <hellodejisol@gmail.com>
2021-04-25 15:41:43 +0100

    Update project

---
+++ python/projects/block-letters/initials.py
@@ -0,0 +1,10 @@
+#Ayodeji Solomon
+'''Experienced Community Builder/Organizer, IT Professional, Software Developer and Tech Educator. Building developer communities with Google, WordPress and Codecademy, passionate community developer and adept at training others. I have the technical know-how to forge interpersonal relationships and I am skilled at planning, hosting virtual, in-person and hybrid events, contributing to organizational growth.'''
+my_initials = '''  A     SSS 
+ A A   S   S
+A   A  S    
+AAAAA   SSS 
+A   A      S
+A   A  S   S
+A   A   SSS '''
+print(my_initials)
